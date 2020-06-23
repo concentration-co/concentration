7ba7d25aea8cc78eb9a72fed4c9303d306734dba
Jennifer Chinzi <jenrose87@gmail.com>
2020-06-23 10:45:24 -0700

    Create JS files for all three pages.  Scaffold JS concepts for app.js

---
+++ js/about.js
@@ -0,0 +1,8 @@
+'use strict';
+
+TODO: // Rendered results will display the User Name, Difficulty, and Final Time
+TODO: // A ‘Play Again’ Option will display beneath the results that - if clicked - will link back to the landing page.
+
+TODO: // Results Page will include an ‘About Me’ sidebar that will display images and names of all 4 collaborators
+TODO: // When clicked, each image/name will expand to display text of a personalized ‘About Me’ written by that collaborator.
+TODO: // Only one full ‘About Me’ text can be displayed at a given time - clicking on a different picture will replace the current text with the newly selected text
---
+++ js/game.js
@@ -0,0 +1,15 @@
+'use strict';
+
+TODO: // Render the board with the number and style of cards selected on the landing page
+TODO: // Render a sidebar that notes all preference choices along with User Name and a timer that is set to 0:00 and will start upon first click
+
+TODO: // Timer begins counting when the first card is clicked
+TODO: // When first card is clicked, card is ‘flipped’ to display the content (but no match is checked)
+TODO: // When a second card is clicked, card is ‘flipped’ to display the content && a match between the first and second card is checked for
+
+TODO: // IF the two flipped cards are a match, both cards are removed from gameplay
+TODO: // IF the two flipped cards are NOT a match, both cards will flip back to their original state and will remain a part of the game
+
+TODO: // Timer stops when the last pair is confirmed as a match and all cards have been removed from gameplay
+TODO: // Final time is saved to local storage
+TODO: // Users are transported to the final Result Page upon successful completion of the game.
