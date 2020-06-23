af1a4dc21cd747015f0ce2f4ffb0aba522b75add
Paul OBrien <pvovideo@outlook.com>
2020-06-23 16:25:02 -0700

    add User constructor, submit button. Issue #8

--- js/app.js
+++ js/app.js
@@ -49,7 +49,33 @@
 // TODO: Provide a button with text indicating that it will ‘Begin the Game’
 // -Target HTML ID ___________ where the button to begin the game will exist
 
-// TODO: When selected, 'Begin the Game' button will save all indicated choices to local storage and then move the user to the Game page
+function User(name, difficulty, theme) {
+  this.name = name;
+  this.difficulty = difficulty;
+  this.theme = theme;
+  this.finalTimes = []; // where to collect their times/scores
+}
+
+var gameStart = document.getElementById('nameForm');
+gameStart.addEventListener('submit', beginningTheGame);
+function beginningTheGame(click){
+  click.preventDefault();
+
+  var newUsername = click.target.fname.value; // OR User.name = ...
+  var newThemeSelect = click.target.theme.value; // OR User.name = ...
+  var newDifficultySelect = click.target.mode.value; // OR difficultySelect = ...
+
+  var newUser = new User(newUsername, newDifficultySelect, newThemeSelect);
+
+  var stringifiedUser = JSON.stringify(newUser);
+  localStorage.setItem('userData', stringifiedUser);
+
+  alert('Okay ' + newUser.name + ' you are ready to play the game? It starts soon as you push the button!');
+
+  window.location.href = 'game.html';
+}
+
+// When selected, 'Begin the Game' button will save all indicated choices to local storage and then move the user to the Game page
 // -stringify the User Object
 // -set User Object to local storage
 // -redirect user to game.html
