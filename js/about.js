f55ab28294b6baa07bd2456c065cf6836c632043
Jennifer Chinzi <jenrose87@gmail.com>
2020-06-23 11:37:53 -0700

    Add details to about.js and first half of game.js

--- js/about.js
+++ js/about.js
@@ -1,8 +1,14 @@
 'use strict';
 
-TODO: // Rendered results will display the User Name, Difficulty, and Final Time
+TODO: // Fill results will display the User Name, Difficulty, and Final Time
+// -Destringify User Object from local storage
+// -Parse User Object from local storage to access User Name, Difficulty and High Score
+// -Add relevant content to results form on about.html
+
 TODO: // A ‘Play Again’ Option will display beneath the results that - if clicked - will link back to the landing page.
+// Use same button as in game.js => Anchor Tag to index.html
 
 TODO: // Results Page will include an ‘About Me’ sidebar that will display images and names of all 4 collaborators
+
 TODO: // When clicked, each image/name will expand to display text of a personalized ‘About Me’ written by that collaborator.
-TODO: // Only one full ‘About Me’ text can be displayed at a given time - clicking on a different picture will replace the current text with the newly selected text
+//- Only one full ‘About Me’ text can be displayed at a given time - clicking on a different picture will replace the current text with the newly selected text
--- js/game.js
+++ js/game.js
@@ -1,15 +1,37 @@
 'use strict';
 
-TODO: // Render the board with the number and style of cards selected on the landing page
-TODO: // Render a sidebar that notes all preference choices along with User Name and a timer that is set to 0:00 and will start upon first click
+// TODO: Card Constructor must exist (Source - url - front of card , Theme - var - back of card)
+
+TODO: // Fill the board with the number and style of cards selected on the landing page
+// -Retrieve and parse User Object from local storage
+// -Fill game board with Style and Difficulty settings from User Object
+// -Based on Difficulty, fill Array A with Difficulty # of Cards from Card Constructor
+// -Fill Array B with an exact copy of Array A cards
+// -Pull from Array A & B to fill game grid
+
+TODO: // Append name & difficulty & button (Go Home) in a sidebar
+// -Preferences will be retrieved from parsed User Object (already destringifyed above)
+
+// TODO: Render a timer that is set to 0:00 and will start upon first click.
+// - Timer will be located in the sidebar
 
 TODO: // Timer begins counting when the first card is clicked
+// -Add Event Listener that targets 'click' on any card
+// -IF timer is not running && click occurs && pairs remain in play, Turn On Timer
+// -Else IF timer IS running && click occurs && NO pairs remain in play, Turn Off Timer
+// -Not Necessary Yet: Else if timer IS running & click occurs && pairs remain in play, nothing happens
+
 TODO: // When first card is clicked, card is ‘flipped’ to display the content (but no match is checked)
+// -Add Event Listener on 'click' on any card => Card Flips
+
 TODO: // When a second card is clicked, card is ‘flipped’ to display the content && a match between the first and second card is checked for
 
 TODO: // IF the two flipped cards are a match, both cards are removed from gameplay
+
 TODO: // IF the two flipped cards are NOT a match, both cards will flip back to their original state and will remain a part of the game
 
 TODO: // Timer stops when the last pair is confirmed as a match and all cards have been removed from gameplay
+
 TODO: // Final time is saved to local storage
+
 TODO: // Users are transported to the final Result Page upon successful completion of the game.
